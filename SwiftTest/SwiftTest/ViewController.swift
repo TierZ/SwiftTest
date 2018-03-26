f5a8aa024a23894bb3e0e951fcb61bcdd719e0f8
TierZ <zuoxiaodong1989@126.com>
2018-03-26 15:18:54 +0800

    first commit

--- SwiftTest/SwiftTest/ViewController.swift
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // swift 练习
         // Do any additional setup after loading the view, typically from a nib.
     }
 
